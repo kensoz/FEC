a2d9f5b4f052978541b35203b522986e531c371a
kensoz <kensozlian@gmail.com>
2022-11-16 17:17:51 +0900

    update: 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,34 @@
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+
 // 404ページ
 const Custom404 = () => {
   return (
-    <div>
-      <div>404</div>
-    </div>
+    <section className='h-full flex justify-center items-center pb-20'>
+      <div className='flex flex-col font-bold'>
+        <div className='text-gray-400 text-xl mb-5'>
+          <span className='mr-2 text-5xl'>404</span>page not found
+        </div>
+
+        <div>お探しのページは見つけることはできません</div>
+
+        <div>
+          无法找到指定页面 <span className='ml-1'>😥</span>
+        </div>
+
+        <div className='mt-3 text-xs'>
+          <Link href='/' passHref>
+            <a className='inline-flex items-center px-4 py-2 rounded-md shadow-sm text-gray-500 bg-gray-100 hover:bg-gray-200 hover:text-gray-600'>
+              <span>
+                <FontAwesomeIcon className='mr-2' icon={faHome} />
+              </span>
+              <span>HOME</span>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
