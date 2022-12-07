de744ad2f1f9cd5145ba5749e0ce5920d4319b9e
kensoz <kensozlian@gmail.com>
2022-12-07 18:45:31 +0900

    update unit test

--- jest-shim.js
+++ jest-shim.js
@@ -1,4 +1,4 @@
-// Jestセットアップ
+// Jest セットアップ
 // https://www.appsloveworld.com/reactjs/100/9/referenceerror-textencoder-is-not-defined-when-running-react-scripts-test-e
 // [SOLVED]-`REFERENCEERROR: TEXTENCODER IS NOT DEFINED` WHEN RUNNING `REACT-SCRIPTS TEST --ENV=JSDOM`-REACTJS
 import { TextDecoder, TextEncoder } from 'util'
