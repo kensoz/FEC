11c26cdbfaa4038e142b75169686e7f79c7e55dc
kensoz <kensozlian@gmail.com>
2022-12-09 15:18:15 +0900

    featrue: add sitemap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,8 +11,8 @@ const Document = (): JSX.Element => {
       {/* Head */}
       <Head>
         <link rel='icon' href='/favicon.ico' />
-        <link rel='shortcut icon' href='/logo-s.png' />
-        <link rel='apple-touch-icon' sizes='200x200' href='/logo-s.png' />
+        <link rel='shortcut icon' href='/icon.png' />
+        <link rel='apple-touch-icon' sizes='200x200' href='/icon.png' />
         <meta name='apple-mobile-web-app-title' content='fec' />
         <meta name='description' content='fec Front-End-Collection フロントエンドコレクション' />
         <link href='https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500;700;800;900&display=swap' rel='stylesheet' />
